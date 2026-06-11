79343ec7f374497368f4c3f7ab0f04b018089193
AndreaDan99 <andrea.dantona@unife.it>
2026-06-11 15:10:18 +0100

    fix: use frame_id world (not link00) as user requested

--- src/spot_control/spot_control/test_exposure_poses.py
+++ src/spot_control/spot_control/test_exposure_poses.py
@@ -701,7 +701,7 @@ class ExposurePoseTester(Node):
             cz = float(ep.camera_xyz[2])
 
         goal = PoseStamped()
-        goal.header.frame_id = 'link00'
+        goal.header.frame_id = 'world'
         goal.header.stamp = self.get_clock().now().to_msg()
         goal.pose.position.x = cx
         goal.pose.position.y = cy
@@ -724,7 +724,7 @@ class ExposurePoseTester(Node):
 
     def _send_home(self):
         msg = PoseStamped()
-        msg.header.frame_id = 'link00'
+        msg.header.frame_id = 'world'
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.pose.position.x = float(HOME_POS[0])
         msg.pose.position.y = float(HOME_POS[1])
@@ -748,7 +748,7 @@ class ExposurePoseTester(Node):
 
     def _publish_virtual_skeleton(self):
         pa = PoseArray()
-        pa.header.frame_id = 'link00'
+        pa.header.frame_id = 'world'
         pa.header.stamp = self.get_clock().now().to_msg()
         for i in range(NUM_JOINTS):
             pose = Pose()
@@ -772,7 +772,7 @@ class ExposurePoseTester(Node):
         for i, ep in enumerate(self._points):
             cr, cg, cb = REGION_COLORS.get(ep.region, (0.5, 0.5, 0.5))
             m = Marker()
-            m.header.frame_id = 'link00'
+            m.header.frame_id = 'world'
             m.header.stamp = self.get_clock().now().to_msg()
             m.ns = f'exposure_grid_{ep.region.value}'
             m.id = i
